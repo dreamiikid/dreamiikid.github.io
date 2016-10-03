2859dc5d3745a7bf4366db2dea2e816627c6a4e5
dreamiikid <18kuokhh2@stconline.edu.hk>
2016-10-03 11:45:11 +0800

    uploaded files

---
+++ poke/js/index.js
@@ -0,0 +1,13 @@
+function snel(){
+  alert ("m i cute")
+}
+function piplup(){
+  alert ("hai m piplup")
+}
+function mew(){
+  alert ("oi dun ignore me im mew")
+}
+function saveme(){
+  document.getElementbyId('hellochild').style.height = "400px";
+  console.log("i have seen many things");
+}
